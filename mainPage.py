896cc46c9a49fb6978b5898e67e4f8869d60a430
GaEun132 <rkdms5813@gmail.com>
2025-08-04 10:12:01 +0900

    시각화 feature 수정

--- mainPage.py
+++ mainPage.py
@@ -255,12 +255,12 @@ def main():
     dftd = pd.read_csv("BE/TrainDataAll.csv")
 
     # Feature 목록
-    features = [                        
-                    "url_entropy",
-                    "url_path_len", "url_filename_len", "url_longest_dom_token_len",
+    features = [            
+                        
+                    "url_entropy","url_starts_with_www",
+                    "url_longest_dom_token_len",
                     "url_average_dom_token_len", "url_domain_len", "url_hostname_len", 
-                    "url_port",
-                    "script","div"
+                    "script","div","a","span"
                 ]
     if page == "메인":
         st.title("지키링")
